da2eab87d8db8cfc7c645e750eb6c62a31db46be
Daniel <wowmegadenis@hotmail.com>
2016-06-05 16:09:02 -0300

    Add validation object

---
+++ blorm-lib/src/main/java/br/com/bloder/blorm/validation/Validation.java
@@ -0,0 +1,15 @@
+package br.com.bloder.blorm.validation;
+
+import android.view.View;
+
+/**
+ * Created by bloder on 05/06/16.
+ */
+public abstract class Validation {
+
+  public View field;
+
+  public Validate validate() {
+    return null;
+  }
+}
