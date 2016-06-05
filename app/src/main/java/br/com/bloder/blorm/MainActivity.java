3e64b4c71bf848b4f8ab2459467c9b78ba98a11f
Daniel <wowmegadenis@hotmail.com>
2016-06-05 18:28:11 -0300

    Add edit text and button example in main activity

--- app/src/main/java/br/com/bloder/blorm/MainActivity.java
+++ app/src/main/java/br/com/bloder/blorm/MainActivity.java
@@ -2,12 +2,25 @@ package br.com.bloder.blorm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import br.com.bloder.blormlib.Blorm;
+import br.com.bloder.blormlib.validation.Validations;
 
 public class MainActivity extends AppCompatActivity {
+
+  private EditText editTextFilled;
+  private Button submit;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    editTextFilled = (EditText) findViewById(R.id.edit_text_filled);
+    submit = (Button) findViewById(R.id.submit);
+
+    new Blorm.Builder(this).field(editTextFilled).is(Validations.filled).onSubmit(submit);
   }
 }
