920265692e09c99876fda7cd12fa4db73d95e2c4
bloder <wowmegadenis@hotmail.com>
2016-06-07 16:52:46 -0300

    Fix check box bug

--- blormlib/src/main/java/br/com/bloder/blormlib/validation/validations/Checked.java
+++ blormlib/src/main/java/br/com/bloder/blormlib/validation/validations/Checked.java
@@ -14,6 +14,7 @@ public class Checked extends Validation {
         return new Validate() {
             @Override
             public boolean validate() {
+               checkBox = (CheckBox) field;
                return checkBox.isChecked();
             }
 
