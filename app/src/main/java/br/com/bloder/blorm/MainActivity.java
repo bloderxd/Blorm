8e19099e87e3c94e57d6f01ae29f1e0a1cebdfaa
Daniel <wowmegadenis@hotmail.com>
2016-07-15 02:35:51 -0300

    Add blorm example to main activity

--- app/src/main/java/br/com/bloder/blorm/MainActivity.java
+++ app/src/main/java/br/com/bloder/blorm/MainActivity.java
@@ -1,7 +1,7 @@
 package br.com.bloder.blorm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -10,15 +10,18 @@ import android.widget.Toast;
 import br.com.bloder.blormlib.Blorm;
 import br.com.bloder.blormlib.validation.Action;
 import br.com.bloder.blormlib.validation.Validate;
-import br.com.bloder.blormlib.validation.Validation;
 
-import static br.com.bloder.blormlib.validation.Validations.*;
+import static br.com.bloder.blormlib.validation.Validations.checked;
+import static br.com.bloder.blormlib.validation.Validations.filled;
 
 public class MainActivity extends AppCompatActivity {
 
   private EditText editTextFilled;
+  private EditText editTextFilled2;
+  private EditText editTextFilled3;
   private Button submit;
   private CheckBox checkBox;
+  private CheckBox checkBox2;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +29,33 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     editTextFilled = (EditText) findViewById(R.id.edit_text_filled);
-    submit = (Button) findViewById(R.id.submit);
+    editTextFilled2 = (EditText) findViewById(R.id.edit_text_filled_2);
+    editTextFilled3 = (EditText) findViewById(R.id.edit_text_filled_3);
     checkBox = (CheckBox) findViewById(R.id.check_box);
-
-    editTextFilled.setText("Hello");
+    checkBox2 = (CheckBox) findViewById(R.id.check_box2);
+    submit = (Button) findViewById(R.id.submit);
 
       new Blorm.Builder()
-              .field(editTextFilled).is(filled)
+              .validate(new Validate() {
+        @Override
+        public boolean validate() {
+          return editTextFilled != null && !editTextFilled.getText().toString().isEmpty();
+        }
+
+        @Override
+        public void onError() {
+          editTextFilled.setError("RONALDO");
+        }
+
+        @Override
+        public void onSuccess() {
+          editTextFilled.setError(null);
+        }
+      })
+              .field(editTextFilled2).is("test2", filled)
+              .andField(editTextFilled3).is(filled)
               .andField(checkBox).is(checked)
+              .andField(checkBox2).is("test3", checked)
               .onSuccess(new Action() {
                   @Override
                   public void call() {
