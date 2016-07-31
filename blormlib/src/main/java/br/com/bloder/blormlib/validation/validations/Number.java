a5d6324484b31f00e9c3d789400c73f65165450e
Daniel <wowmegadenis@hotmail.com>
2016-07-31 14:49:25 -0300

    Add number validation logic

---
+++ blormlib/src/main/java/br/com/bloder/blormlib/validation/validations/Number.java
@@ -0,0 +1,37 @@
+package br.com.bloder.blormlib.validation.validations;
+
+import android.text.TextUtils;
+import android.widget.EditText;
+
+import br.com.bloder.blormlib.validation.Validate;
+import br.com.bloder.blormlib.validation.Validation;
+
+/**
+ * Created by bloder on 31/07/16.
+ */
+public class Number extends Validation {
+
+  EditText editText;
+
+  @Override
+  public Validate validate() {
+    return new Validate() {
+      @Override
+      public boolean validate() {
+        editText = (EditText) field;
+        if (editText.getText().toString().isEmpty()) return false;
+        return TextUtils.isDigitsOnly(editText.getText().toString());
+      }
+
+      @Override
+      public void onError() {
+        editText.setError("This field only accept numbers!");
+      }
+
+      @Override
+      public void onSuccess() {
+
+      }
+    };
+  }
+}
